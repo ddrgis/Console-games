630993aec1ad7039923aa6d332d48c4a7888b963
ddrgis <kasabiankpp@gmail.com>
2017-12-12 23:17:47 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
-    console.log("Welcome to the Brain Games!");
-    const name = readlineSync.question('What is your name: ');
-    console.log(`Hello, ${name}!`);
-}
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('What is your name: ');
+  console.log(`Hello, ${name}!`);
+};
