d0fbdaf45b4e2188f8ce9070b4c369c83e67909b
ddrgis <kasabiankpp@gmail.com>
2017-12-15 21:56:20 +0200

    Implement brain-gcd-game and publish npm package

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -1,6 +1,25 @@
 import { createRiddle, rightAnswer } from '../riddle';
 import getRandomInt from '../prng';
 
-export const getRules = () => '';
-export const getRiddle = () => {};
+const MIN_NUMBER = -20;
+const MAX_NUMBER = 20;
+
+const gcd = (a, b) => {
+  if (b === 0) {
+    return Math.abs(a);
+  }
+  return gcd(b, a % b);
+};
+
+const getQuestion = (num1, num2) => `${num1} ${num2}`;
+const getRightAnswer = (num1, num2) => gcd(num1, num2);
+
+export const getRules = () => 'Find the greatest common divisor of given numbers.';
+export const getRiddle = () => {
+  const num1 = getRandomInt(MIN_NUMBER, MAX_NUMBER);
+  const num2 = getRandomInt(MIN_NUMBER, MAX_NUMBER);
+  const question = getQuestion(num1, num2);
+  const correctAnswer = getRightAnswer(num1, num2);
+  return createRiddle(question, correctAnswer);
+};
 export const isRight = (riddle, playerAnswer) => rightAnswer(riddle) === parseInt(playerAnswer, 10);
