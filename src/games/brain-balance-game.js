ea6c3baf150ce852f3d99687450e5d0f908a931e
ddrgis <kasabiankpp@gmail.com>
2017-12-15 23:07:27 +0200

    Add balance-game methods except balanceNumber

--- src/games/brain-balance-game.js
+++ src/games/brain-balance-game.js
@@ -1,10 +1,20 @@
 import { createRiddle, rightAnswer } from '../riddle';
 import getRandomInt from '../prng';
 
-export const getRules = () => 'Balance the given number.';
+const MIN_NUMBER = 10;
+const MAX_NUMBER = 9999;
 
+const balanceNumber = num => num;
+
+const getQuestion = num => `${num}`;
+const getRightAnswer = num => balanceNumber(num);
+
+export const getRules = () => 'Balance the given number.';
 export const getRiddle = () => {
-  return createRiddle();
+  const num = getRandomInt(MIN_NUMBER, MAX_NUMBER);
+  const question = getQuestion(num);
+  const correctAnswer = getRightAnswer(num);
+  return createRiddle(question, correctAnswer);
 };
 
 export const isRight = (riddle, playerAnswer) => rightAnswer(riddle) === parseInt(playerAnswer, 10);
