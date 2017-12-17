81587b2f90139ddbc6fd94e162f59e60f4a180d7
ddrgis <kasabiankpp@gmail.com>
2017-12-17 10:17:42 +0200

    Add brain-prime implementation

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -1,19 +1,29 @@
-// import { createRiddle } from '../riddle';
-// import getRandomInt from '../prng';
-// import { startGame } from '../engine';
+import { createRiddle } from '../riddle';
+import getRandomInt from '../prng';
+import { startGame } from '../engine';
 
-// const MIN_NUMBER = -20;
-// const MAX_NUMBER = 20;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 50;
 
+const isPrime = (num) => {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
-// const getQuestion = 
-// const getRightAnswer = 
+const getQuestion = num => `Is this number prime? ${num}`;
+const getRightAnswer = num => (isPrime(num) ? 'yes' : 'no');
 
-// export const getRules = () => 
-// export const getRiddle = () => {
-// const num1 = getRandomInt(MIN_NUMBER, MAX_NUMBER);
-  
-// };
-// export const start = () => {
-//   startGame(getRules, getRiddle);
-// };
+export const getRules = () => 'Answer "yes" if number is prime otherwise answer "no".';
+export const getRiddle = () => {
+  const num = getRandomInt(MIN_NUMBER, MAX_NUMBER);
+  const question = getQuestion(num);
+  const rightAnswer = getRightAnswer(num);
+  return createRiddle(question, rightAnswer);
+};
+export const start = () => {
+  startGame(getRules, getRiddle);
+};
