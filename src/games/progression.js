6874497a84171c52bc0511596e92b763a88bee9c
ddrgis <kasabiankpp@gmail.com>
2017-12-17 15:00:31 +0200

    Refactor progression.js. Get rid of arrays

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,32 +7,30 @@ const MAX_STEP = 20;
 const MIN_START_NUMBER = 0;
 const MAX_START_NUMBER = 20;
 const STEPS = 10;
-
 const getQuestion = progression => `What number is missing in this progression?\n${progression}`;
-const createProgression = () => {
-  const startNum = getRandomInt(MIN_START_NUMBER, MAX_START_NUMBER);
-  const step = getRandomInt(MIN_STEP, MAX_STEP);
-  const iter = (currentNum, counter, acc) => {
-    if (counter >= STEPS) {
-      return acc;
-    }
-    const lastNum = currentNum + step;
-    const newAcc = `${acc}${lastNum} `;
-    const newCounter = counter + 1;
-
-    return iter(lastNum, newCounter, newAcc);
-  };
-  return iter(startNum, 1, `${startNum} `);
-};
 
 export const getRules = () => 'What number is missing in a progression';
-export const getRiddle = () => {
-  const progression = createProgression();
+const getRiddle = () => {
+  const startNum = getRandomInt(MIN_START_NUMBER, MAX_START_NUMBER);
+  const step = getRandomInt(MIN_STEP, MAX_STEP);
   const missedNumIndex = getRandomInt(1, STEPS);
-  const progressionArray = progression.split(' ');
-  const correctAnswer = progressionArray[missedNumIndex];
-  progressionArray[missedNumIndex] = '..';
-  return createRiddle(getQuestion(progressionArray.join(' ')), correctAnswer);
+  const iter = (currentNum, counter, acc, missedNum) => {
+    if (counter >= STEPS) {
+      return createRiddle(getQuestion(acc), missedNum.toString());
+    }
+    const lastNum = currentNum + step;
+    const newCounter = counter + 1;
+
+    if (counter === missedNumIndex) {
+      const newMissedNum = lastNum;
+      const newAcc = `${acc}.. `;
+      return iter(lastNum, newCounter, newAcc, newMissedNum);
+    }
+    const newAcc = `${acc}${lastNum} `;
+    return iter(lastNum, newCounter, newAcc, missedNum);
+  };
+
+  return iter(startNum, 1, `${startNum} `, undefined);
 };
 export const start = () => {
   startGame(getRules, getRiddle);
