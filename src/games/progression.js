5386d22965c9cdbbceef081b853b54ae1265d1c4
ddrgis <kasabiankpp@gmail.com>
2017-12-17 14:37:29 +0200

    Refactor progression.js. Make iterative algorithm instead for loop

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,25 +9,30 @@ const MAX_START_NUMBER = 20;
 const STEPS = 10;
 
 const getQuestion = progression => `What number is missing in this progression?\n${progression}`;
+const createProgression = () => {
+  const startNum = getRandomInt(MIN_START_NUMBER, MAX_START_NUMBER);
+  const step = getRandomInt(MIN_STEP, MAX_STEP);
+  const iter = (currentNum, counter, acc) => {
+    if (counter >= STEPS) {
+      return acc;
+    }
+    const lastNum = currentNum + step;
+    const newAcc = `${acc}${lastNum} `;
+    const newCounter = counter + 1;
+
+    return iter(lastNum, newCounter, newAcc);
+  };
+  return iter(startNum, 1, `${startNum} `);
+};
 
 export const getRules = () => 'What number is missing in a progression';
 export const getRiddle = () => {
-  const step = getRandomInt(MIN_STEP, MAX_STEP);
-  const missedStep = getRandomInt(1, STEPS);
-
-  let currentNumber = getRandomInt(MIN_START_NUMBER, MAX_START_NUMBER);
-  let progression = '';
-  let correctAnswer;
-  for (let i = 0; i < STEPS; i += 1) {
-    if (i === missedStep) {
-      correctAnswer = currentNumber;
-      progression += '.. ';
-    } else {
-      progression += `${currentNumber} `;
-    }
-    currentNumber += step;
-  }
-  return createRiddle(getQuestion(progression), correctAnswer.toString());
+  const progression = createProgression();
+  const missedNumIndex = getRandomInt(1, STEPS);
+  const progressionArray = progression.split(' ');
+  const correctAnswer = progressionArray[missedNumIndex];
+  progressionArray[missedNumIndex] = '..';
+  return createRiddle(getQuestion(progressionArray.join(' ')), correctAnswer);
 };
 export const start = () => {
   startGame(getRules, getRiddle);
